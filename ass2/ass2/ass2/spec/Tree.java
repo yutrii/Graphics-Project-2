809900587d4b098832a7e123d655c527dbd3343b
wiltonxie <wiltonsw500@hotmail.com>
2017-09-27 14:34:24 +1000

    started drawing trees

--- ass2/ass2/ass2/spec/Tree.java
+++ ass2/ass2/ass2/spec/Tree.java
@@ -28,12 +28,68 @@ public class Tree {
     
     public void drawTree(GL2 gl) {
     	
-    	gl.glColor4d(0.247, 0.749, 0.247, 1);
+    	gl.glColor4d(0.6, 0.4, 0.2, 1); //brown
+    	gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+    	gl.glBegin(GL2.GL_QUADS);
+    	
+    	
+    	
+    	double x = myPos[0];
+    	double y = myPos[1];
+    	double z = myPos[2];
+    	
+    	gl.glVertex3d(x + 0.1, y, z);
+    	gl.glVertex3d(x - 0.1, y, z);
+    	gl.glVertex3d(x - 0.1, y + 1.5, z);
+    	gl.glVertex3d(x + 0.1, y + 1.5, z);
+    	
+    	gl.glVertex3d(x, y, z + 0.1);
+    	gl.glVertex3d(x, y, z - 0.1);
+    	gl.glVertex3d(x, y + 1.5, z - 0.1);
+    	gl.glVertex3d(x, y + 1.5, z + 0.1);
+    	
+    	gl.glEnd();
+    	gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+    	
+    	
+    	gl.glColor4d(0.247, 0.749, 0.247, 1); //green
+		
+		gl.glBegin(GL2.GL_POLYGON);    	
+		
+		double angle = 0;
+    	double increment = 360/32;
+    	for (int i = 0; i <= 32; i++, angle += increment) {
+    		
+    		double Z = 0.5 * Math.cos(Math.toRadians(angle));
+    		double Y = 0.5 * Math.sin(Math.toRadians(angle));
+    	
+    		gl.glVertex3d(x, y + 1 + Y, z + Z);
+    	}
+    	
+    	gl.glEnd();
+    	
+    	gl.glBegin(GL2.GL_POLYGON);
+    	
+    	for (int i = 0; i <= 32; i++, angle += increment) {
+    		
+    		double X = 0.5 * Math.cos(Math.toRadians(angle));
+    		double Y = 0.5 * Math.sin(Math.toRadians(angle));
+    	
+    		gl.glVertex3d(x + X, y + 1 + Y, z);
+    	}
+    	
+    	gl.glEnd();
+    	
+    	
+    	gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+    	/*
+    	gl.glColor4d(0.247, 0.749, 0.247, 1); //green
     	gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
     	gl.glBegin(GL2.GL_TRIANGLES);
     	
     	gl.glEnd();
     	gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+    	*/
     }
     
 
