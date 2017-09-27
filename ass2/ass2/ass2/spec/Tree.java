96fd0518519130201cec73e99b646bb946fffba2
wiltonxie <wiltonsw500@hotmail.com>
2017-09-27 13:19:03 +1000

    started Tree.java

--- ass2/ass2/ass2/spec/Tree.java
+++ ass2/ass2/ass2/spec/Tree.java
@@ -1,5 +1,11 @@
 package ass2.spec;
 
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jogamp.opengl.GL2;
+
 /**
  * COMMENT: Comment Tree 
  *
@@ -18,6 +24,16 @@ public class Tree {
     
     public double[] getPosition() {
         return myPos;
+    }
+    
+    public void drawTree(GL2 gl) {
+    	
+    	gl.glColor4d(0.247, 0.749, 0.247, 1);
+    	gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
+    	gl.glBegin(GL2.GL_TRIANGLES);
+    	
+    	gl.glEnd();
+    	gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
     }
     
 
