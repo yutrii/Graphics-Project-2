3372b5f0b622add1484d605d375327a381538cb3
yutrii <youtry@live.com.au>
2017-10-22 15:36:24 +1100

    Added feature to pause rain

--- ass2/ass2/ass2/spec/Game.java
+++ ass2/ass2/ass2/spec/Game.java
@@ -85,9 +85,9 @@ public class Game extends JFrame implements GLEventListener, MouseMotionListener
      */
     public static void main(String[] args) throws FileNotFoundException {
     	//Terrain terrain = LevelIO.load(new File(args[0]));
-    	//Terrain terrain = LevelIO.load(new File("ass2/ass2/spec/testb.json"));
+    	Terrain terrain = LevelIO.load(new File("ass2/ass2/spec/testb.json"));
     	//Terrain terrain = LevelIO.load(new File("ass2/ass2/spec/largeTerrain.json"));
-    	Terrain terrain = LevelIO.load(new File("ass2/ass2/spec/testRoads3.json"));
+    	//Terrain terrain = LevelIO.load(new File("ass2/ass2/spec/testRoads3.json"));
     	//Terrain terrain = LevelIO.load(new File("ass2/ass2/spec/testHill.json"));
     	//Terrain terrain = LevelIO.load(new File("ass2/ass2/spec/testWorld.json"));
     	//Terrain terrain = LevelIO.load(new File("ass2/ass2/spec/testLight.json"));
@@ -251,6 +251,8 @@ public class Game extends JFrame implements GLEventListener, MouseMotionListener
 				myTerrain.rainFast();
 			} else if (key == '-') {
 				myTerrain.rainSlow();
+			} else if (key == 'f') {
+				myTerrain.toggleRainPause();
 			}
 			
 		}
